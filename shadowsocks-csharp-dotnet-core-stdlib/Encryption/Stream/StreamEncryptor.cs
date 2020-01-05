dbf82b521b23ca3d045bac97fd677c3754fe37fe
Stzx <silence.m@hotmail.com>
2020-01-05 22:34:49 +0800

    WIP

---
+++ shadowsocks-csharp-dotnet-core-stdlib/Encryption/Stream/StreamEncryptor.cs
@@ -0,0 +1,20 @@
+﻿using Shadowsocks.Std.Encryption.Parameters;
+
+namespace Shadowsocks.Std.Encryption.Stream
+{
+    public abstract class StreamEncryptor : EncryptorBase<StreamEncryptionParameters>
+    {
+        protected byte[] _encryptIV;
+        protected byte[] _decryptIV;
+
+        protected StreamEncryptor(string passwd, StreamEncryptionParameters parameters) : base(passwd, parameters)
+        {
+
+        }
+
+        protected virtual void InitIv(ref byte[] iv)
+        {
+            _random.NextBytes(iv);
+        }
+    }
+}
